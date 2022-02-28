2cbfa49f36a586b5b73c9fb25fa8d47c8239824e
Christian Franchin dos Santos <christianfranchin@gmail.com>
2022-02-28 20:23:09 -0300

    🚑️ (response): Remove unused field

--- backend/src/helpers/response.py
+++ backend/src/helpers/response.py
@@ -1,39 +1,34 @@
 from flask import make_response
 
+
 class DefaultResponse():
     @staticmethod
-    def success_response(message: str = '', data: dict[any] = {}, cookies: dict[str, str] = {}, status: int = 200):
+    def success_response(message: str = '', data: dict[any] = {}, status: int = 200):
         default = {
             'success': True,
             'message': message,
             'data': data
         }
         resp = make_response(default)
-        for key, value in cookies.items():
-            resp.set_cookie(key, value)
 
         return resp, status
-    
+
     @staticmethod
-    def failed_response(message: str = '', cookies: dict[str, str] = {}, status: int = 400):
+    def failed_response(message: str = '', status: int = 400):
         default = {
             'success': False,
             'message': message,
         }
         resp = make_response(default)
-        for key, value in cookies.items():
-            resp.set_cookie(key, value)
 
         return resp, status
 
     @staticmethod
-    def internal_failed_response(message: str = 'Internal error', cookies: dict[str, str] = {}, status: int = 500):
+    def internal_failed_response(message: str = 'Internal error', status: int = 500):
         default = {
             'success': False,
             'message': message,
         }
         resp = make_response(default)
-        for key, value in cookies.items():
-            resp.set_cookie(key, value)
 
         return resp, status
